163a9f34e85a55234ff1f4af4cbc5b0002896749
Ravi Babu Ravela <charanravela1998@gmail.com>
2019-11-27 09:43:58 +0530

    Interface  changes

--- SRC/addstudentFormValidation.php
+++ SRC/addstudentFormValidation.php
@@ -17,14 +17,14 @@ if (isset($_POST['SN']) && isset($_POST['EN']) && isset($_POST['SB']) && isset($
 	$pass="FST";
 	$R="N";
 	$N = "N";
-	echo "<script type ='text/javascript'> alert('$s_name' + ' ' + '$sid' + ' ' + '$branch' + ' ' + '$phno' + ' ' + '$email' + ' ' + '$y' + ' ' + '$s' + ' ' + '$pass' + '' + '$N');</script>";
+//	echo "<script type ='text/javascript'> alert('$s_name' + ' ' + '$sid' + ' ' + '$branch' + ' ' + '$phno' + ' ' + '$email' + ' ' + '$y' + ' ' + '$s' + ' ' + '$pass' + '' + '$N');</script>";
 } else {
     $message = "Some values are missing. Please add again";
     die();
     echo "<script type='text/javascript'>alert('$message'); window.location.href= \"addstudent.php\"</script>";
 }
 $q = mysqli_query(mysqli_connect("localhost", "root", "", "ttms"), "INSERT into student values ('$sid','$s_name','$y','$s','$branch','$pass','$phno','$email','$R','$N')");
-echo "<script type='text/javascript'>alert('$q');</script>";
+//echo "<script type='text/javascript'>alert('$q');</script>";
 if ($q) {
     $message = "Student added.";
     echo "<script type='text/javascript'>alert('$message'); window.location.href= \"addstudent.php\"</script>";
